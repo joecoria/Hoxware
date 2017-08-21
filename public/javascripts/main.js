bf464ace89d0130cb9b4181b99326099525dbdc3
Karen Chizón <karen.chizon@gmail.com>
2017-08-21 10:17:26 -0500

    inicio hoxware

---
+++ public/javascripts/main.js
@@ -0,0 +1,37 @@
+// Scrollify!
+if ($(window).width() < 993) {
+	console.log('chico..?');
+	$.scrollify.destroy();
+} else {
+	console.log('grande?');
+	$.scrollify({
+		section:".panel",
+		scrollSpeed:900,
+		after:function(i) {/* if(i===2) {$.scrollify.setOptions({easing:"easeOutExpo"});} */}
+	});
+};
+// $(".scroll,.scroll-btn").click(function(e) {
+//   e.preventDefault();
+//   $.scrollify.next();
+// });
+// navbar
+
+$('.hoxnav').hide();
+$(window).on("resize scroll", function (){
+  if (Math.round($(window).scrollTop()) > 200) {
+    $('.hoxnav').show("bounce", {direction: "down"}, 1000)
+  }
+	if (Math.round($(window).scrollTop()) == 3) {
+    $('.hoxnav').hide("blind", {direction: "up"}, 1000)
+  }
+});
+
+// signature!
+console.log("%c  #  ·············With Tech & Love·················","color:cyan");
+console.log("%c  #  ···███████╗·█████╗·██████╗·██╗·····███████╗···","color:cyan");
+console.log("%c  #  ···██╔════╝██╔══██╗██╔══██╗██║·····██╔════╝···","color:cyan");
+console.log("%c  #  ···███████╗███████║██████╔╝██║·····█████╗·····","color:cyan");
+console.log("%c  #  ···╚════██║██╔══██║██╔══██╗██║·····██╔══╝·····","color:cyan");
+console.log("%c  #  ██╗███████║██║··██║██████╔╝███████╗███████╗██╗","color:cyan");
+console.log("%c  #  ╚═╝╚══════╝╚═╝··╚═╝╚═════╝·╚══════╝╚══════╝╚═╝","color:cyan");
+console.log("%c  #  ················Digital Studio················","color:cyan");
