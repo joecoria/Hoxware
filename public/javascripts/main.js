7c661c4eba2dc538c61c83ed31d199a202187360
Karen Chizón <karen.chizon@gmail.com>
2017-08-22 14:03:50 -0500

    efecto de entrada logo intro

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -22,8 +22,12 @@ $(window).on("resize scroll", function (){
     $('.logo').show("bounce", {direction: "down"}, 1000)
   }
 	if (Math.round($(window).scrollTop()) == 3) {
-    $('.logo').hide("blind", {direction: "up"}, 1000)
+    $('.logo').hide("blind", {direction: "up"}, 500)
   }
+});
+$( "#hoxware" ).hide();
+$(document).ready(function() {
+  $( "#hoxware" ).show("pulsate");
 });
 
 
