b7a5647b1009a9eb00cfc1c6d83fa9648f0b30bf
Karen Chizón <karen.chizon@gmail.com>
2017-09-07 14:44:17 -0500

    no scrollify on mobile

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,95 +1,100 @@
 // Scrollify!
-
-$.scrollify({
-	section:".panel",
-	scrollSpeed:900,
-	overflowScroll: true,
-  before:function(i,panels) {
-    var ref = panels[i].attr("data-section-name");
-    $(".vnav .active").removeClass("active");
-    $(".vnav").find("a[href=\"#" + ref + "\"]").addClass("active");
-		$.scrollify.update();
-		if($.scrollify.current().attr('data-section-name') === 'danos'){
-			$('.typed').typeIt({
-				strings: ["El daño operativo puede ocasionar incumplimiento en compromisos y servicios.",
-				"El daño económico se sufre al perder clientes y pagar multas de regulaciones comerciales.",
-				"El daño de imagen genera desconfianza y mala reputación de tu organización."],
-				speed: 80,
-				breakLines: false,
-				loopDelay: 3500,
-				deleteDelay: 3500,
-				autoStart: true,
-				loop: true
-			});
-			$('.userinfo:first-child').addClass('moved');
-			$('.userinfo:first-child p.line1, .userinfo:first-child p span.cursor1, .userinfo:first-child p.line2, .userinfo:first-child p span.cursor2, .userinfo:first-child p.line3, .userinfo:first-child p span.cursor3, .userinfo:first-child p.line4, .userinfo:first-child p span.cursor4').addClass('animate');
-			function sube2(){
-				$('.userinfo:nth-child(2)').addClass('moved');
-				$('.userinfo:nth-child(2) p.line1, .userinfo:nth-child(2) p span.cursor1, .userinfo:nth-child(2) p.line2, .userinfo:nth-child(2) p span.cursor2, .userinfo:nth-child(2) p.line3, .userinfo:nth-child(2) p span.cursor3, .userinfo:nth-child(2) p.line4, .userinfo:nth-child(2) p span.cursor4').addClass('animate');
+if ($(window).width() < 993) {
+	// console.log('chico..?');
+	$.scrollify.destroy();
+} else {
+	// console.log('grande?');
+	$.scrollify({
+		section:".panel",
+		scrollSpeed:900,
+		overflowScroll: true,
+	  before:function(i,panels) {
+	    var ref = panels[i].attr("data-section-name");
+	    $(".vnav .active").removeClass("active");
+	    $(".vnav").find("a[href=\"#" + ref + "\"]").addClass("active");
+			$.scrollify.update();
+			if($.scrollify.current().attr('data-section-name') === 'danos'){
+				$('.typed').typeIt({
+					strings: ["El daño operativo puede ocasionar incumplimiento en compromisos y servicios.",
+					"El daño económico se sufre al perder clientes y pagar multas de regulaciones comerciales.",
+					"El daño de imagen genera desconfianza y mala reputación de tu organización."],
+					speed: 80,
+					breakLines: false,
+					loopDelay: 3500,
+					deleteDelay: 3500,
+					autoStart: true,
+					loop: true
+				});
+				$('.userinfo:first-child').addClass('moved');
+				$('.userinfo:first-child p.line1, .userinfo:first-child p span.cursor1, .userinfo:first-child p.line2, .userinfo:first-child p span.cursor2, .userinfo:first-child p.line3, .userinfo:first-child p span.cursor3, .userinfo:first-child p.line4, .userinfo:first-child p span.cursor4').addClass('animate');
+				function sube2(){
+					$('.userinfo:nth-child(2)').addClass('moved');
+					$('.userinfo:nth-child(2) p.line1, .userinfo:nth-child(2) p span.cursor1, .userinfo:nth-child(2) p.line2, .userinfo:nth-child(2) p span.cursor2, .userinfo:nth-child(2) p.line3, .userinfo:nth-child(2) p span.cursor3, .userinfo:nth-child(2) p.line4, .userinfo:nth-child(2) p span.cursor4').addClass('animate');
+				}
+				setTimeout(sube2, 3000);
+				function sube3(){
+					$('.userinfo:nth-child(3)').addClass('moved');
+					$('.userinfo:nth-child(3) p.line1, .userinfo:nth-child(3) p span.cursor1, .userinfo:nth-child(3) p.line2, .userinfo:nth-child(3) p span.cursor2, .userinfo:nth-child(3) p.line3, .userinfo:nth-child(3) p span.cursor3, .userinfo:nth-child(3) p.line4, .userinfo:nth-child(3) p span.cursor4').addClass('animate');
+				}
+				setTimeout(sube3, 6000);
+				function sube4(){
+					$('.userinfo:nth-child(4)').addClass('moved');
+					$('.userinfo:nth-child(4) p.line1, .userinfo:nth-child(4) p span.cursor1, .userinfo:nth-child(4) p.line2, .userinfo:nth-child(4) p span.cursor2, .userinfo:nth-child(4) p.line3, .userinfo:nth-child(4) p span.cursor3, .userinfo:nth-child(4) p.line4, .userinfo:nth-child(4) p span.cursor4').addClass('animate');
+				}
+				setTimeout(sube4, 9000);
+				$('#damagemore').click(function() {
+					$('.userinfo p.line1, .userinfo p span.cursor1, .userinfo p.line2, .userinfo p span.cursor2, .userinfo p.line3, .userinfo p span.cursor3, .userinfo p.line4, .userinfo p span.cursor4').toggleClass('animate');
+					$('.userinfo').toggleClass('moved');
+				});
+			} else if($.scrollify.current().attr('data-section-name') === 'proteccion'){
+				$('#masdedamage.show').removeClass('show');
+				$('.userinfo p.line1, .userinfo p span.cursor1, .userinfo p.line2, .userinfo p span.cursor2, .userinfo p.line3, .userinfo p span.cursor3, .userinfo p.line4, .userinfo p span.cursor4').removeClass('animate');
+				$('.userinfo').removeClass('moved');
+				$('#shieldmore').click(function() {
+					$('#last').toggle();
+				});
+				$('.bin01').typeIt({
+					strings: ['01100001 <span>01101100</span> 01101100 01111001', '01101111 01110101 01110010 01100010', '01100001 01110011 01100101 01100001', '01110010 01100101 01100010 01100101', '01101100 01101111 01101110 01100111', '01110100 01101111 01110101 01110011'],
+					speed: 50,
+					startDelay: 500,
+					autoStart: true
+				});
+				$('.bin02').typeIt({
+					strings: ['01100001 01101100 01101100 01111001', '01101111 01110101 01110010 01100010', '01100001 01110011 01100101 01100001', '<span>01110010</span> 01100101 01100010 01100101', '01101100 01101111 01101110 01100111', '01110100 01101111 01110101 01110011'],
+					speed: 50,
+					startDelay: 3000,
+					autoStart: true
+				});
+				$('.bin03').typeIt({
+					strings: ['01100001 <span>01101100</span> 01101100 01111001', '01101111 01110101 01110010 01100010', '01100001 01110011 01100101 01100001', '01110010 01100101 <span>01100010</span> 01100101', '01101100 01101111 01101110 01100111', '01110100 01101111 01110101 01110011'],
+					speed: 50,
+					startDelay: 6000,
+					autoStart: true
+				});
+			} else if($.scrollify.current().attr('data-section-name') === 'contacto'){
+				$('#masproteccion.show').removeClass('show');
+			} else {
+				$('#masinfo.show').removeClass('show');
+				$('#masdedamage.show').removeClass('show');
+				$('.userinfo p.line1, .userinfo p span.cursor1, .userinfo p.line2, .userinfo p span.cursor2, .userinfo p.line3, .userinfo p span.cursor3, .userinfo p.line4, .userinfo p span.cursor4').removeClass('animate');
+				$('.userinfo').removeClass('moved');
 			}
-			setTimeout(sube2, 3000);
-			function sube3(){
-				$('.userinfo:nth-child(3)').addClass('moved');
-				$('.userinfo:nth-child(3) p.line1, .userinfo:nth-child(3) p span.cursor1, .userinfo:nth-child(3) p.line2, .userinfo:nth-child(3) p span.cursor2, .userinfo:nth-child(3) p.line3, .userinfo:nth-child(3) p span.cursor3, .userinfo:nth-child(3) p.line4, .userinfo:nth-child(3) p span.cursor4').addClass('animate');
-			}
-			setTimeout(sube3, 6000);
-			function sube4(){
-				$('.userinfo:nth-child(4)').addClass('moved');
-				$('.userinfo:nth-child(4) p.line1, .userinfo:nth-child(4) p span.cursor1, .userinfo:nth-child(4) p.line2, .userinfo:nth-child(4) p span.cursor2, .userinfo:nth-child(4) p.line3, .userinfo:nth-child(4) p span.cursor3, .userinfo:nth-child(4) p.line4, .userinfo:nth-child(4) p span.cursor4').addClass('animate');
-			}
-			setTimeout(sube4, 9000);
-			$('#damagemore').click(function() {
-				$('.userinfo p.line1, .userinfo p span.cursor1, .userinfo p.line2, .userinfo p span.cursor2, .userinfo p.line3, .userinfo p span.cursor3, .userinfo p.line4, .userinfo p span.cursor4').toggleClass('animate');
-				$('.userinfo').toggleClass('moved');
-			});
-		} else if($.scrollify.current().attr('data-section-name') === 'proteccion'){
-			$('#masdedamage.show').removeClass('show');
-			$('.userinfo p.line1, .userinfo p span.cursor1, .userinfo p.line2, .userinfo p span.cursor2, .userinfo p.line3, .userinfo p span.cursor3, .userinfo p.line4, .userinfo p span.cursor4').removeClass('animate');
-			$('.userinfo').removeClass('moved');
-			$('#shieldmore').click(function() {
-				$('#last').toggle();
-			});
-			$('.bin01').typeIt({
-				strings: ['01100001 <span>01101100</span> 01101100 01111001', '01101111 01110101 01110010 01100010', '01100001 01110011 01100101 01100001', '01110010 01100101 01100010 01100101', '01101100 01101111 01101110 01100111', '01110100 01101111 01110101 01110011'],
-				speed: 50,
-				startDelay: 500,
-				autoStart: true
-			});
-			$('.bin02').typeIt({
-				strings: ['01100001 01101100 01101100 01111001', '01101111 01110101 01110010 01100010', '01100001 01110011 01100101 01100001', '<span>01110010</span> 01100101 01100010 01100101', '01101100 01101111 01101110 01100111', '01110100 01101111 01110101 01110011'],
-				speed: 50,
-				startDelay: 3000,
-				autoStart: true
-			});
-			$('.bin03').typeIt({
-				strings: ['01100001 <span>01101100</span> 01101100 01111001', '01101111 01110101 01110010 01100010', '01100001 01110011 01100101 01100001', '01110010 01100101 <span>01100010</span> 01100101', '01101100 01101111 01101110 01100111', '01110100 01101111 01110101 01110011'],
-				speed: 50,
-				startDelay: 6000,
-				autoStart: true
-			});
-		} else if($.scrollify.current().attr('data-section-name') === 'contacto'){
-			$('#masproteccion.show').removeClass('show');
-		} else {
-			$('#masinfo.show').removeClass('show');
-			$('#masdedamage.show').removeClass('show');
-			$('.userinfo p.line1, .userinfo p span.cursor1, .userinfo p.line2, .userinfo p span.cursor2, .userinfo p.line3, .userinfo p span.cursor3, .userinfo p.line4, .userinfo p span.cursor4').removeClass('animate');
-			$('.userinfo').removeClass('moved');
-		}
-  },
-  afterRender:function() {
-    var vnav = "<ul class=\"vnav\">";
-    var activeClass = "";
-    $(".panel").each(function(i) {
-      activeClass = "";
-      if(i===0) {
-        activeClass = "active";
-      }
-      vnav += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
-    });
-    vnav += "</ul>";
-    $("a.vlink").on("click",$.scrollify.move);
-  }
-});
+	  },
+	  afterRender:function() {
+	    var vnav = "<ul class=\"vnav\">";
+	    var activeClass = "";
+	    $(".panel").each(function(i) {
+	      activeClass = "";
+	      if(i===0) {
+	        activeClass = "active";
+	      }
+	      vnav += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+	    });
+	    vnav += "</ul>";
+	    $("a.vlink").on("click",$.scrollify.move);
+	  }
+	});
+};
 
 // navbar
 $('.logo').hide();
